65ab68949468058babe693b65fc0d5828b69b6ca
jakovistuk <jistuk@gmail.com>
2020-02-12 10:42:38 +0100

    Update app.js

--- node-example/app.js
+++ node-example/app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const os = require('os');
 
-console.log("Kubia server starting...");
+console.log("server starting...");
 
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("VERZIJA 33333333 test-poc You've hit " + os.hostname() + "\n");
+  response.end("VERZIJA 44444444444 test-poc You've hit " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
