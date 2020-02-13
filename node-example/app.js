a504dac477d7e44f7c8c2127931be9886b5412f9
jakovistuk <jistuk@gmail.com>
2020-02-13 10:29:36 +0100

    Update app.js

--- node-example/app.js
+++ node-example/app.js
@@ -4,9 +4,10 @@ const os = require('os');
 console.log("server starting...");
 
 var handler = function(request, response) {
+  var datetime = new Date();
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("VERZIJA 44444444444 test-poc You've hit " + os.hostname() + "\n");
+  response.end("VERZIJA 55555555555 test-poc You've hit " + os.hostname() + "\n"+datetime);
 };
 
 var www = http.createServer(handler);
