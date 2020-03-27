0001c522a8f4e7d4d2b21894d4af54f0eed18506
jakovistuk <jistuk@gmail.com>
2020-03-27 11:56:57 +0100

    Create foldercreate2.groovy

---
+++ foldercreate2.groovy
@@ -0,0 +1,7 @@
+//////////////////////////////////////////
+// PRIMJER DEFINIRANJA FOLDERA
+///////////////////////////////////////////
+folder('somedummyfolder2') {
+    displayName "XXX Some dummy folder Display Name"
+    description "XXXX Some dummy folder Description"
+}
